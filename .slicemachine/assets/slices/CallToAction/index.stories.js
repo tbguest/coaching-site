4fe2ce899bf3e3496537211e25a7a8b2ca98ec19
Tristan Guest <tristanguest@gmail.com>
2023-06-04 16:42:15 -0300

    Fix images, set defaults

--- .slicemachine/assets/slices/CallToAction/index.stories.js
+++ .slicemachine/assets/slices/CallToAction/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"call_to_action","items":[],"primary":{"description":[{"type":"paragraph","text":"Ullamco veniam ad duis officia aliqua.","spans":[]}],"buttonText":[{"type":"paragraph","text":"Consectetur quis excepteur mollit reprehenderit cupidatat ut laborum elit consequat voluptate voluptate consectetur laboris. Lorem mollit irure irure incididunt esse mollit labore quis enim pariatur.","spans":[]}],"link":{"link_type":"Web","url":"http://google.com"}},"id":"_Default"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"call_to_action","items":[],"primary":{"description":[{"type":"paragraph","text":"Incididunt nisi officia in sint dolor ex exercitation laborum incididunt dolore. Cillum labore Lorem in qui nostrud deserunt velit et aute voluptate qui consectetur. Anim ex duis ea laborum deserunt commodo deserunt nulla ea reprehenderit aliquip proident tempor.","spans":[]}],"buttonText":[{"type":"paragraph","text":"Do do eiusmod do dolor ipsum officia. Nisi sunt ad sunt laboris Lorem proident elit aliqua eu pariatur Lorem.","spans":[]}],"link":{"link_type":"Web","url":"https://slicemachine.dev"}},"id":"_Default"}} />
 _Default.storyName = 'Default'
